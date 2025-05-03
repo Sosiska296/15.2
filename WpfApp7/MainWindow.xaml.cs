312cf8bcdb6b575543002abc63e7371b3c6c7387
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-05-04 00:37:21 +0500

    Добавьте файлы проекта.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7;
+    public partial class MainWindow : Window
+    {
+        private List<SolidColorBrush> colors;
+        private Random rnd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            colors = new List<SolidColorBrush>
+            {
+                Brushes.Red,
+                Brushes.Green,
+                Brushes.Blue,
+                Brushes.Yellow,
+                Brushes.Orange
+            };
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var buttons = new List<Button> { Button1, Button2, Button3, Button4, Button5 };
+            SolidColorBrush previousColor = null;
+            foreach (var button in buttons)
+            {
+                SolidColorBrush newColor;
+                do
+                {
+                    newColor = colors[rnd.Next(colors.Count)];
+                } while (newColor == previousColor);
+                button.Background = newColor; 
+                previousColor = newColor;
+        }
+        // как пожарить яичницу: Налить растительное масло в сковороду и поставить на огонь. Разбить яйца и вылить их на сковород. 
+        // Посолить по вкусу. Жарить яичницу необходимо 2–3 минуты.
+    }
+}
